bbefb03b0f10dad9b1a7141307418f24677730a2
Yannik Sembritzki <yannik@sembritzki.me>
2020-04-23 17:03:04 +0200

    Initial commit

---
+++ files/ZZ-shell-customization.sh
@@ -0,0 +1,37 @@
+# This file is getting sourced, therefore no hashbang
+
+# Not an interactive shell?
+[[ $- == *i* ]] || return 0
+
+EDITOR=vim
+
+source /usr/local/bin/hcmnt
+hcmntextra='date "+%d.%m.%Y %H:%M:%S"'
+
+hcmnt_cmd() {
+    hcmnt -eityl ~/.hcmnt.log $LOGNAME@$HOSTNAME
+}
+
+[ -n "$ZSH_VERSION" ] && precmd_functions+=(hcmnt_cmd)
+
+# only bash stuff after this
+[ -n "$BASH_VERSION"  ] || return 0
+
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}hcmnt_cmd"
+
+bind '"\e[A"':history-search-backward
+bind '"\e[B"':history-search-forward
+
+# Avoid duplicates
+HISTCONTROL=ignoredups:erasedups
+# When the shell exits, append to the history file instead of overwriting it (https://unix.stackexchange.com/questions/6501)
+shopt -s histappend
+
+# make .bash_history unlimited size
+HISTSIZE=
+HISTFILESIZE=
+
+# After each command, append to the history file and reread it
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+#src: https://unix.stackexchange.com/questions/1288/preserve-bash-history-in-multiple-terminal-windows/1292#1292
+
